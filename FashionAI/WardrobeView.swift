2035ecca496a079e596a5cc1b4c3bbae80c2e389
Ulugbek Abdimurodov <u.abdimurodov@studenti.unisa.it>
2024-10-15 12:15:05 +0200

    v1 wardrobe

---
+++ FashionAI/WardrobeView.swift
@@ -0,0 +1,18 @@
+//
+//  WardrobeView.swift
+//  FashionAI
+//
+//  Created by Ulugbek Abdimurodov on 15/10/24.
+//
+
+import SwiftUI
+
+struct WardrobeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WardrobeView()
+}
