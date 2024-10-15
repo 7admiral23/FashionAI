88ab8fae282ae1c1aac9926b7bfb823ab155cca1
Ulugbek Abdimurodov <u.abdimurodov@studenti.unisa.it>
2024-10-15 09:51:14 +0200

    Initial Commit

---
+++ FashionAI/FashionAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionAIApp.swift
+//  FashionAI
+//
+//  Created by Ulugbek Abdimurodov on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FashionAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
